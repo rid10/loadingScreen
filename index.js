777973ee9b0ace4a77ee2c1baf020dd99b7b493e
rid <rid.sin@gmail.com>
2019-07-03 09:21:09 +0530

    Callbacks and Error Handling

---
+++ index.js
@@ -0,0 +1,15 @@
+function solveRect(l,b){
+	console.log("Solving rectangle with l= "+l+"and b= "+y);
+	 rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
